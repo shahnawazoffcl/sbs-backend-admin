5b244539ac2ba91937d4388e463fdd812c302232
shahnawazoffcl <shahnawaz0901khan@gmail.com>
2025-04-02 23:35:06 +0530

    added variables for db connection

--- src/main/java/com/admin/sbsbackend/security/CorsConfig.java
+++ src/main/java/com/admin/sbsbackend/security/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Allow all origins
-                .allowedOrigins("https://sbs-admin-main.vercel.app/login","https://super-dusk-800533.netlify.app")  // Replace with your Angular app URL
+                .allowedOrigins("https://sbs-admin-main.vercel.app","https://super-dusk-800533.netlify.app")  // Replace with your Angular app URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")  // Allowed HTTP methods
                 .allowedHeaders("*")
                 .allowCredentials(true)  // Allow cookies and credentials
